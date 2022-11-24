96b8cd0e4663383a9b9366d57d8626e858d40198
zeevhan <80751180+Zeev1988@users.noreply.github.com>
2022-11-24 16:03:44 +0200

    Update segmentation_export.py

--- nnunet/inference/segmentation_export.py
+++ nnunet/inference/segmentation_export.py
@@ -186,7 +186,7 @@ def save_segmentation_nifti(segmentation, out_fname, dct, order=1, force_separat
 
     if np.any(np.array(current_shape) != np.array(shape_original_after_cropping)):
         if order == 0:
-            seg_old_spacing = resize_segmentation(segmentation, shape_original_after_cropping, 0, 0)
+            seg_old_spacing = resize_segmentation(segmentation, shape_original_after_cropping, 0)
         else:
             if force_separate_z is None:
                 if get_do_separate_z(dct.get('original_spacing')):
